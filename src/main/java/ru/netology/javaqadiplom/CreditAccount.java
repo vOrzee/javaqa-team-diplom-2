5dcc2d9dc680ef8cb43f5fa7a024eef1a418baf3
NikitaLeon <159457430+NikitaLeon@users.noreply.github.com>
2024-10-09 11:33:13 +0300

    Fix #3 #4 #5

--- src/main/java/ru/netology/javaqadiplom/CreditAccount.java
+++ src/main/java/ru/netology/javaqadiplom/CreditAccount.java
@@ -17,8 +17,8 @@ public class CreditAccount extends Account {
      * @param rate - неотрицательное число, ставка кредитования для расчёта долга за отрицательный баланс
      */
     public CreditAccount(int initialBalance, int creditLimit, int rate) {
-        if (rate <= 0 || creditLimit <= 0 || initialBalance <=0) {
-            throw new IllegalArgumentException(
+        if (rate <= 0 || creditLimit <= 0 || initialBalance <=0) { // Добавлены поля для кредитного лимита и
+            throw new IllegalArgumentException(                    //     и текущего баланса
                     "Накопительная ставка не может быть отрицательной, а у вас: " + rate
             );
         }
@@ -42,10 +42,10 @@ public class CreditAccount extends Account {
             return false;
         }
         balance = balance - amount;
-        if (balance > -creditLimit) {
-            balance = -amount;
+        if (balance >= -creditLimit) {   //Больше или равен, а не простобольше
             return true;
         } else {
+            balance += amount;           //В случае false, а не true
             return false;
         }
     }
